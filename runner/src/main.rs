da149e79703c2f6d773b0c7f7bced06acd959226
Rez Bahmanpour <b.mohammadreza@gmail.com>
2026-01-02 20:54:30 -0500

    Parse perflab args.

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,3 +1,34 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about=None)]
+#[command(propagate_version=true)]
+struct CmdLine {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Runs the benchmark. Options: --bench <name>, --compiler <name> -- <flags...>
+    Run {
+        #[arg(short, long, value_name = "name")]
+        bench: String,
+
+        #[arg(short, long, value_name = "path")]
+        compiler: String,
+
+        #[arg(trailing_var_arg=true, allow_hyphen_values=true)]
+        compiler_args: Vec<String>,
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cl = CmdLine::parse();
+
+    match cl.command {
+        Commands::Run {bench, compiler, compiler_args} => { 
+            println!("WIP - bench<{bench}>, compiler<{compiler}>, flags<{:?}>", compiler_args);
+        }
+    }
 }
